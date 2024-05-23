b1674fc9b015e22ae18f767630ed129c8c02f65f
泽健 周 <z1210333872@outlook.com>
2024-05-23 18:59:54 +0800

    siro test

--- src/AntSK.Domain/Common/DependencyInjection/JesseExtensions.cs
+++ src/AntSK.Domain/Common/DependencyInjection/JesseExtensions.cs
@@ -123,6 +123,65 @@ namespace AntSK.Domain.Common.DependencyInjection
                 return _memory;
             });
 
+            services.AddKeyedSingleton("ocrMemory", (services, _) =>
+            {
+                var searchClientConfig = new SearchClientConfig
+                {
+                    MaxAskPromptSize = 1024,
+                    MaxMatchesCount = 10,
+                    AnswerTokens = 1024,
+                    EmptyAnswer = KmsConstantcs.KmsSearchNull
+                };
+
+                string pyDll = services.GetRequiredService<IOptions<LocalBgeConfigOptions>>().Value.PythonDllPath;
+                string bgeEmbeddingModelName = services.GetRequiredService<IOptions<LocalBgeConfigOptions>>().Value.EmbeddingModel;
+                var hostingEnvironment = services.GetRequiredService<IWebHostEnvironment>();
+
+                var memoryBuild = new KernelMemoryBuilder()
+                                        .WithDashScopeTextGeneration(new Cnblogs.KernelMemory.AI.DashScope.DashScopeConfig
+                                        {
+                                            ApiKey = "N/A",
+                                        })
+                                        //.WithoutDefaultHandlers()
+                                        .WithSearchClientConfig(searchClientConfig)
+                                        .WithBgeTextEmbeddingGeneration(new HuggingfaceTextEmbeddingGenerator(pyDll, bgeEmbeddingModelName))
+                                        .WithSimpleVectorDb(new SimpleVectorDbConfig()
+                                        {
+                                            StorageType = FileSystemTypes.Volatile
+                                        });
+
+                var _memory = memoryBuild.Build<MemoryServerless>();
+
+                _memory.Orchestrator.AddHandler<TextExtractionHandler>("extract_text");
+                _memory.Orchestrator.AddHandler<KMExcelHandler>("antsk_excel_split");
+                _memory.Orchestrator.AddHandler<GenerateEmbeddingsHandler>("generate_embeddings");
+                _memory.Orchestrator.AddHandler<SaveRecordsHandler>("save_memory_records");
+
+                var fileName = @"202405221320-TESTS.xlsx";
+                var filePath = Path.Combine(hostingEnvironment.WebRootPath, "files", fileName);
+                using FileStream fileStream = System.IO.File.OpenRead(filePath);
+
+                var dataTable = ExcelHelper.ExcelToDataTable(fileStream, true);
+
+                StringBuilder text = new StringBuilder();
+                foreach (DataRow item in dataTable.Rows)
+                {
+                    text.AppendLine(@$"{item["TESTNO"].ToString()}{KmsConstantcs.KMExcelSplit}");
+                }
+                var importText = text.ToString();
+                _memory.ImportTextAsync(importText,
+                      steps: new[]
+                      {
+                                        "extract_text",
+                                        "antsk_excel_split",
+                                        "generate_embeddings",
+                                        "save_memory_records"
+                      }
+                ).Wait();
+
+                return _memory;
+            });
+
             return services;
         }
     }
